b72e4702debcd27acdf8572dcc1e10a7e811657c
kanouken <1462927100@qq.com>
2016-01-14 15:12:41 +0800

    init import

---
+++ SeaWeedFSClient/src/main/java/com/onestong/component/open/SeaWeedFSClient/App.java
@@ -0,0 +1,13 @@
+package com.onestong.component.open.SeaWeedFSClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
